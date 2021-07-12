d74ed850bd4fa2581c360f8a9b45617767c686d7
Rodrigo <rvargas@fi.uba.ar>
2021-07-12 04:08:42 -0300

    testDado

--- src/main/test/edu/fiuba/algo3/modelo/DadosTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/DadosTest.java
@@ -8,10 +8,11 @@ public class DadosTest {
     public void test01DevuelveListaDeDadosVacia() {
         Dados dados = new Dados();
         assertEquals(dados.dadosAtaque(0).size(), 0);
+        assertEquals(dados.dadosDefensa(0).size(), 0);
     }
 
     @Test
-    public void test02DevuelveListaDeDadosNoVacia() {
+    public void test02DevuelveListaDeDadosDeAtaqueNoVacia() {
         Dados dados = new Dados();
         assertEquals(dados.dadosAtaque(1).size(), 0);
         assertEquals(dados.dadosAtaque(2).size(), 1);
@@ -23,12 +24,27 @@ public class DadosTest {
     }
 
     @Test
+    public void test02DevuelveListaDeDadosDeDefensaNoVacia() {
+        Dados dados = new Dados();
+        assertEquals(dados.dadosDefensa(1).size(), 2);
+        assertEquals(dados.dadosDefensa(2).size(), 3);
+        assertEquals(dados.dadosDefensa(3).size(), 3);
+        assertEquals(dados.dadosDefensa(4).size(), 3);
+        assertEquals(dados.dadosDefensa(5).size(), 3);
+        assertEquals(dados.dadosDefensa(0).size(), 0);
+    }
+
+    @Test
     public void test03DevuelveResultadosDeUnDadoReal() {
         Dados dados = new Dados();
-        assertEquals(dados.dadosAtaque(4).size(), 3);
         System.out.println(dados.dadosAtaque(4));
         System.out.println(dados.dadosAtaque(2));
         System.out.println(dados.dadosAtaque(3));
         System.out.println(dados.dadosAtaque(1));
+
+        System.out.println(dados.dadosDefensa(4));
+        System.out.println(dados.dadosDefensa(5));
+        System.out.println(dados.dadosDefensa(1));
+        System.out.println(dados.dadosDefensa(2));
     }
 }
