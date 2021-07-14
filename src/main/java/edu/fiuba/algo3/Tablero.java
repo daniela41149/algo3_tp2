80d873d65b09c44b9adb58ee544d09aca0988c7d
Lucia <llourengo@fi.uba.ar>
2021-07-13 21:39:42 -0300

    tablero y test

--- src/main/java/edu/fiuba/algo3/Tablero.java
+++ src/main/java/edu/fiuba/algo3/Tablero.java
@@ -1,5 +1,45 @@
 package edu.fiuba.algo3;
 
 
+import java.util.ArrayList;
+import java.lang.String;
+import java.util.List;
+import java.io.*;
+
 public class Tablero {
+    //private private List<Continente> continentes;
+    private List<Pais> paises;
+
+
+    /*atacar(string string cant ejercito)*/
+
+    private List<Integer> leerArchivo (String rutaArchivo){
+        List<Integer> listaPaises = new ArrayList<>();
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(rutaArchivo));
+            String unRenglon = br.readLine();
+            while(unRenglon != null)
+            {
+                String[] parts = unRenglon.split(",");
+                String pais = parts[0];
+                String continente = parts[1];
+                List<String> nombrePaisesLimitrofes = parts[2];
+                Pais nuevoPais = new Pais(pais, continente, nombrePaisesLimitrofes);
+                listaPaises.add (nuevoPais);
+                unRenglon = br.readLine();
+            }
+        }
+        finally {
+                if (br != null)
+                    br.close();
+        }
+        return listaPaises;
+    }
+
+    public Tablero(String rutaArchivo){
+        //this.continentes = ;
+        this.paises = leerArchivo(rutaArchivo) ;
+    }
+
 }
---
+++ src/main/test/edu/fiuba/algo3/modelo/TableroTest.java
@@ -0,0 +1,16 @@
+package edu.fiuba.algo3.modelo;
+
+
+import edu.fiuba.algo3.Tablero;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TableroTest {
+    @Test
+    public void test01LaCantidadDePaisesEs50() {
+       Tablero tablero = new Tablero("Fronteras.csv");
+        assertEquals(tablero.paises.size(), 50);
+
+    }
+
+}
