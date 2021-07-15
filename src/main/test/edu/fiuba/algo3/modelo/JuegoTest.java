e436403b1d68ecb6ed6d1dd1f97ad2376e89bcce
Julian Galvan <jhgalvan@fi.uba.ar>
2021-07-15 20:32:54 -0300

    Actualizacion de Juego y JuegoTest

--- src/main/test/edu/fiuba/algo3/modelo/JuegoTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/JuegoTest.java
@@ -1,22 +1,27 @@
 package edu.fiuba.algo3.modelo;
 
-import edu.fiuba.algo3.CantidadInvalidaDeJugadoresException;
-import edu.fiuba.algo3.Juego;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 public class JuegoTest {
-
+/*
     @Test
-    public void test01NoSePuedeCrearUnJuegoConMenosDeDosJugadores() {
+    public void test01NoSePuedeAgregarAUnJuegoMasDeSieteJugadores() throws SuperaMaximoDeJugadoresException {
+        Juego juego = new Juego();
 
-        assertThrows(CantidadInvalidaDeJugadoresException.class, () -> new Juego(1));
+        juego.agregarJugador("jugador1");
+        juego.agregarJugador("jugador2");
+        juego.agregarJugador("jugador3");
+        juego.agregarJugador("jugador4");
+        juego.agregarJugador("jugador5");
+        juego.agregarJugador("jugador6");
+
+        assertThrows(SuperaMaximoDeJugadoresException.class, () -> juego.agregarJugador("jugador7"));
     }
 
     @Test
-    public void test02NoSePuedeCrearUnJuegoConMasDeSeisJugadores() {
+    public void test02NoSePuedeComenzarLaFaseInicialDeUnJuegoConMenosDeDosJugadores() throws SuperaMaximoDeJugadoresException {
+        Juego juego = new Juego();
 
-        assertThrows(CantidadInvalidaDeJugadoresException.class, () -> new Juego(7));
+        juego.agregarJugador("jugador1");
+        assertThrows(NoSeSuperaMinimoDeJugadoresException.class, () -> juego.comenzarFaseInicial());
     }
+    */
 }
