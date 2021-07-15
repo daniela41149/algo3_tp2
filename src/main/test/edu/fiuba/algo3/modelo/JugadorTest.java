cff8a21f14cb55f49153c8ad1415b017e70fe1ac
Lucia <llourengo@fi.uba.ar>
2021-07-15 09:56:20 -0300

    jugadorTest

---
+++ src/main/test/edu/fiuba/algo3/modelo/JugadorTest.java
@@ -0,0 +1,15 @@
+package edu.fiuba.algo3.modelo;
+
+
+
+import edu.fiuba.algo3.Jugador;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JugadorTest {
+    @Test
+    public void test01LaCantidadDePaisesEs50() {
+
+
+    }
+}
