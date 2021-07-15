aaaf7a5df1f7d12500f60c6e6562804df220481e
Lucia <llourengo@fi.uba.ar>
2021-07-15 16:08:02 -0300

    correccion de jugador

--- src/main/java/edu/fiuba/algo3/Jugador.java
+++ src/main/java/edu/fiuba/algo3/Jugador.java
@@ -1,11 +1,19 @@
 package edu.fiuba.algo3;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Jugador {
     private String nombreJugador;
     private String color;
+    private List<Pais> paises = new ArrayList<>();
+
+    public Jugador(String nombre, String colorJugador, List<Pais> paisesDelJugador){
+        this.nombreJugador = nombre;
+        this. color = colorJugador;
+        this. paises = paisesDelJugador;
+    }
 
 
     public void jugarTurno(){
@@ -16,9 +24,26 @@ public class Jugador {
         Scanner cantEjercito = new Scanner(System.in);
         int cantEjercitoAtaca = cantEjercito.nextInt();
 
-        atacar(paisAtacanteNombre,paisDefensorNombre,cantEjercitoAtaca);
+
+        //atacar(paisAtacanteNombre,paisDefensorNombre,cantEjercitoAtaca);
+
     }
-    public void desocupar(){
+    public boolean esElMismo(Jugador jugador){
+        return (jugador.nombreJugador.equals(this.nombreJugador);
+    }
+    public void desocupar(String nombrePais){
+        boolean paisEncontrado = false;
+        int i = 0;
+        Pais paisBuscado;
+        while( (!paisEncontrado) && (i < paises.size()) ){
+            paisBuscado = paises[i];
+
+            if(paisBuscado.coincideNombre(nombrePais)){
+                paisEncontrado = true;
+                paises.remove(i);
+            }
+            i++;
+        }
 
     }
 }
