b36253bb88b3da6a26df937a28eafe1b88ffe957
Julian Galvan <jhgalvan@fi.uba.ar>
2021-07-16 13:37:59 -0300

    Actualizacion de Juego

--- src/main/java/edu/fiuba/algo3/Juego.java
+++ src/main/java/edu/fiuba/algo3/Juego.java
@@ -1,7 +1,9 @@
 package edu.fiuba.algo3;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 public class Juego {
 
@@ -24,29 +26,28 @@ public class Juego {
         String colorAsignado = COLORES[jugadores.size()];
         jugadores.add(new Jugador(nombreDeJugador, colorAsignado, this));
     }
-/*
+
     private void repartirPaisesAleatoriamente(int cantidadPorJugador) {
         Random rand = new Random();
-        List<Pais> paisesSinRepartir = tablero.obtenerPaises;
+        ArrayList<Pais> paisesSinRepartir = new ArrayList<>(tablero.pasarPiasesAJuego());
 
-        for (int i = 0; i < jugadores.size(); i++) {
+        for (Jugador jugadorActual : jugadores) {
             for (int j = 0; j < cantidadPorJugador; j++) {
                 int posicionAleatoria = rand.nextInt(paisesSinRepartir.size());
                 Pais paisAleatorio = paisesSinRepartir.get(posicionAleatoria);
 
-                jugadores.get(i).agregarPais(paisAleatorio);
+                jugadorActual.agregarPais(paisAleatorio);
                 paisesSinRepartir.remove(paisAleatorio);
             }
         }
     }
-*/
+
     public void comenzarFaseInicial() throws NoSeSuperaMinimoDeJugadoresException {
         if (jugadores.size() < MIN_JUGADORES)
             throw new NoSeSuperaMinimoDeJugadoresException();
-        /*
+
         int paisesPorJugador = (tablero.cantidadPaises()/jugadores.size());
         repartirPaisesAleatoriamente(paisesPorJugador);
-        */
     }
 
     public void comenzarFaseDeJuego() {
