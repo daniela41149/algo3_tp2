90aef8e9b4e3ace2ca781f27581ca54ec0d288f8
Rodrigo <rvargas@fi.uba.ar>
2021-07-18 08:42:08 -0300

    .

--- src/main/java/edu/fiuba/algo3/controladorDados.java
+++ src/main/java/edu/fiuba/algo3/controladorDados.java
@@ -1,5 +1,29 @@
 package edu.fiuba.algo3;
 
-public class controladorDados {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class controladorDados implements Initializable {
+
+
+    @FXML
+    private ImageView dadoAtacanteUno;
+    private Image caraUno = new Image( getClass().getResourceAsStream( "dice_1.png" ) );
     
+    @FXML
+    public void desplegarImagenesDeDados(){
+        dadoAtacanteUno.setImage(caraUno);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        
+    }
+
 }
+    
