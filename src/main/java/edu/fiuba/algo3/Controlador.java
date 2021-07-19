34fa1724312456e7786d1c22932ebe4ae7cba497
Rodrigo <rvargas@fi.uba.ar>
2021-07-19 00:04:20 -0300

    controlador con fallas

---
+++ src/main/java/edu/fiuba/algo3/Controlador.java
@@ -0,0 +1,36 @@
+package edu.fiuba.algo3;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class Controlador {
+
+    @FXML
+    private Button boton;
+
+    @FXML
+    private ImageView dadoAtacanteUno;
+
+    @FXML
+    private ImageView dadoAtacanteDos;
+
+    @FXML
+    private ImageView dadoAtacanteTres;
+
+    @FXML
+    private ImageView dadoDefensorUno;
+
+    @FXML
+    private ImageView dadoDefensorDos;
+
+    @FXML
+    private ImageView dadoDefensorTres;
+
+    @FXML
+    void lanzar(ActionEvent event) {
+
+    }
+
+}
